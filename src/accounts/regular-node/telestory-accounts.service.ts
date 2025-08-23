ad778dfeacb5504afb9a7d11d084ba7be775f37c
asimaranov <a.simaranov@gmail.com>
2025-08-23 13:31:01 +0300

    Fixed formtting

--- src/accounts/regular-node/telestory-accounts.service.ts
+++ src/accounts/regular-node/telestory-accounts.service.ts
@@ -469,7 +469,7 @@ export class TelestoryAccountsService implements OnModuleInit {
                     .slice(0, 5) // Limit to first 5
                     .map((account) => {
                       const phoneDisplay = account.phone
-                        ? `***${account.phone.slice(-4)}`
+                        ? `\*\*\*${account.phone.slice(-4)}`
                         : 'номер не указан';
                       const reason =
                         account.inactiveReason || 'причина не указана';
